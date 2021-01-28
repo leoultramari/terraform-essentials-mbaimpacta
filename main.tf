94490b5f15bf3d548fe35feca04cd0d5ebddefb4
Leonardo Ultramari <leonardoultramari@gmail.com>
2021-01-27 22:21:07 -0300

    Aplicando boas práticas no código

--- main.tf
+++ main.tf
@@ -1,27 +1,8 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
-  }
-}
-
-# Configura o Provider Google Cloud com o Projeto
-provider "google" {
-  version = "3.5.0"
-
-  #credentials = file("/vagrant/leonardo-mbaimpacta-turma04-732c8e471b85.json")
-
-  project     = "leonardo-cloudautomation"
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
-
 # Cria uma VM no Google Cloud
 resource "google_compute_instance" "firstvm" {
   name         = "website"
   machine_type = "n1-standard-1"
-  zone         = "us-central1-a"
+  zone         = var.gcp_zone
 
   # Insira essa linha após zone 
   tags         = ["website", "impacta"]
@@ -41,9 +22,4 @@ resource "google_compute_instance" "firstvm" {
       // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
     }
   }
-}
-
-# Retorna o IP da VM criada
-output "ip" {
-  value = google_compute_instance.firstvm.network_interface.0.access_config.0.nat_ip
 }
---
+++ varitables.auto.tfvars
@@ -0,0 +1,2 @@
+# Variáveis do GCP
+gcp_project   = "leonardo-cloudautomation"
