9217c6d3386fdea675d723ece95d6f0dacb9245d
Leonardo Ultramari <leonardoultramari@gmail.com>
2021-03-08 00:39:41 -0300

    Atualizando Providers

--- provider.tf
+++ provider.tf
@@ -2,14 +2,13 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
+      version = "3.58.0"
     }
   }
 }
 
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
-  version = "3.5.0"
-
   project = var.gcp_project
   region  = var.gcp_region
   zone    = var.gcp_zone
