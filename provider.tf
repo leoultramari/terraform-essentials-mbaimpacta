4772033f734d48a498fdcde6f789ffed93b2ca62
Leonardo Ultramari <leonardoultramari@gmail.com>
2021-03-09 09:55:37 -0300

    Adicionando Cloudflare

--- provider.tf
+++ provider.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/google"
       version = "3.58.0"
     }
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "2.18.0"
+    }
   }
 }
 
@@ -12,4 +16,10 @@ provider "google" {
   project = var.gcp_project
   region  = var.gcp_region
   zone    = var.gcp_zone
+}
+
+# Configura o Provider CloudFlare
+provider "cloudflare" {
+  email   = var.cloudflare_email
+  api_key = var.cloudflare_api_key
 }
